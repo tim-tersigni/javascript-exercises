eebd980b665d7ddadf74aaacf196a3ad34357832
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-02 23:50:29 -0500

    Complete 04 through 06

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  result = (temp - 32) * (5/9);
+  rounded = Math.round(result * 10) / 10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  result = temp * (9/5) + 32;
+  rounded = Math.round(result * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
