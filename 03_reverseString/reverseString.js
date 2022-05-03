d4e15b2fee2a274165b0a5e9674ddf2192caf276
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-02 22:52:52 -0500

    Complete 03_REVERSESTRING

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+// COMPLETED 05/02/2022
 
+const reverseString = function(input) {
+  out = '';
+  for(const char of input) {
+    console.log(char);
+    out = char + out;
+  }
+  return out;
 };
 
 // Do not edit below this line
