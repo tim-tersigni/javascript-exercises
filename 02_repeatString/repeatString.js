57730a453f61647f4f431a2cd8584bd22e235f7e
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-02 19:20:12 -0500

    Finish repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(given, times) {
+    out = ""
 
+    if (times < 0) {
+        return "ERROR";
+    }
+
+    for (count = 0; count < times; count++) {
+        out += given;
+    }
+    return out;
 };
 
 // Do not edit below this line
