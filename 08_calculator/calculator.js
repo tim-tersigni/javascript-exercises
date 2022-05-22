78c219dd99c4e903250d3bd3757ad67e65118b70
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-22 13:20:02 -0500

    Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	arr.forEach((i) => {
+    sum += i;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let res = arr[0];
+  for(let i = 1; i < arr.length; i++) {
+    res = res * arr[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x == 0 || x == 1) {
+    return 1;
+  }
+  else if (x > 0) {
+    return x * factorial(x - 1);
+  }
 };
 
 // Do not edit below this line
