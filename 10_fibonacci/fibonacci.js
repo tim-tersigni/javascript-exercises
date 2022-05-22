11fd173df3c7daa33a439e72873542f390a283f6
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-22 17:52:41 -0500

    fibbonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+function fibHelper(x1, x2, i, num) {
+  if (i >= num - 2) return x1 + x2;
 
+  return fibHelper(x2, x1+x2, i+1, num);
+}
+
+const fibonacci = function(num) {
+  if (num == 0) return 0;
+  else if (num < 0) return 'OOPS'
+  return fibHelper(0, 1, 0, num);
 };
+
+console.log(fibonacci(1))
 
 // Do not edit below this line
 module.exports = fibonacci;
