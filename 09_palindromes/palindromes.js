1bc56df181f422e03019ec25bee7a0981a662005
Tim Tersigni <tim.tersigni@gmail.com>
2022-05-22 17:27:14 -0500

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,40 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  let left = [];
+  let right = [];
 
+  text = text.toLowerCase();
+
+  // remove puncutation or spaces
+  let splitText = text.split('');
+  let newTextArr = [];
+  let removables = [' ', '!', '.', ','];
+  splitText.forEach((letter, i) => {
+    let removed = false;
+    removables.forEach((r) => {
+      if (letter === r) {
+        removed = true;
+      }
+    });
+
+    if (removed == false) {
+      newTextArr.push(letter);
+    }
+  });
+  text = newTextArr.join('');
+  console.log(`processed text: ${text}`);
+
+  // divide word into left and right for comparison
+  for (let i = 0; i < text.length / 2; i++) {
+    left.push(newTextArr[i]);
+    right.push(newTextArr[newTextArr.length - (i + 1)]);
+  }
+  console.log(`left:${left} right:${right}`);
+
+  // compare
+  if (left.every((val, i) => val === right[i])) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
